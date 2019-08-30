9a571736d36c54ecbbbb57e57d8839b278b47677
root <aju.n.narain@gmail.com>
2019-08-30 16:23:30 +0530

    Updates

---
+++ precache-manifest.1f27cec44fec06e5756f31bef873343f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "593573f9d15265f771a9cccefe4d77c9",
+    "url": "/my_app/index.html"
+  },
+  {
+    "revision": "cfb3f3d83b16b682b1a5",
+    "url": "/my_app/static/css/main.bfbdc4fa.chunk.css"
+  },
+  {
+    "revision": "b562d0b36eed5f229e11",
+    "url": "/my_app/static/js/2.fade7d78.chunk.js"
+  },
+  {
+    "revision": "cfb3f3d83b16b682b1a5",
+    "url": "/my_app/static/js/main.77996aa8.chunk.js"
+  },
+  {
+    "revision": "3340181d283c48414f34",
+    "url": "/my_app/static/js/runtime~main.8a9bd57d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_app/static/media/logo.5d5d9eef.svg"
+  }
+]);
